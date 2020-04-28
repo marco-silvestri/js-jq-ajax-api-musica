c163eae14bb5f4cc5dd4e6b165f5efe33a7bd2c0
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-28 16:38:50 +0200

    Genre Selection

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 	// jQuery refs
 	var cdArea = $('.cds-container');
-	var cdCard =$('.cd');
+	var genreSelector = $('#genre');
 
 	// Init
 	var myApi = "https://flynn.boolean.careers/exercises/api/array/music";
@@ -21,19 +21,51 @@ $(document).ready(function() {
 				var albumNumber = data.response.length;
 				for (var i = 0; i < albumNumber; i++){
 					var thisAlbum = data.response[i];
-					var pushAlbum = {
-						cdCard : 'cd',
-						cdThumbnail : thisAlbum.poster,
-						cdTitle : thisAlbum.title,
-						cdAuthor : thisAlbum.author,
-						cdYear : thisAlbum.year
-					};
-					var output = template(pushAlbum);
-					cdArea.append(output);
+					fillAlbum(thisAlbum);
 				}
 			}
 		}
 	});
+
+	genreSelector.change(function () { 
+		var cdCard = $('.cd');
+		var currentGenre = genreSelector.val();
+		console.log(currentGenre);
+		cdCard.remove();
+		$.ajax({
+			type: "GET",
+			url: myApi,
+			success: function (data) {
+				if(data.success){
+					// Retrieve albums number and print a card accordingly to its data
+					var albumNumber = data.response.length;
+					for (var i = 0; i < albumNumber; i++){
+						var thisAlbum = data.response[i];
+						if (thisAlbum.genre.toLowerCase() == currentGenre){
+							fillAlbum(thisAlbum);
+						}
+					}
+				}
+			}
+		});	
+	});
+
+
+/************ 
+* Functions
+************/
+
+function fillAlbum(thisAlbum){
+	var pushAlbum = {
+		cdCard : 'cd',
+		cdThumbnail : thisAlbum.poster,
+		cdTitle : thisAlbum.title,
+		cdAuthor : thisAlbum.author,
+		cdYear : thisAlbum.year
+	};
+	var output = template(pushAlbum);
+	cdArea.append(output);
+}
 
 
 }); //END of DOC READY
